12002823c21c0aa4f30bce871ebd738790bf2ba8
harrydehix <harry.holly02@gmail.com>
2021-08-16 17:27:30 +0200

    Add detailed format information to temperature units

--- src/units/groups/temperature.ts
+++ src/units/groups/temperature.ts
@@ -1,15 +1,23 @@
 import Group from "../../Group";
 import Formats from "../../unit/formatting/Formats";
-import PrefixedUnit from "../../unit/PrefixedUnit";
 import SimpleUnit from "../../unit/SimpleUnit";
 
 const temperature = new Group("temperature");
 
 temperature.setUnits(
-    new SimpleUnit(new Formats(["K"]), (val) => val, (val) => val),
-    new SimpleUnit(new Formats(["°C"]), (val) => val + 273.15, (val) => val - 273.15),
-    new SimpleUnit(new Formats(["°F"]), (val) => (val - 32) * 5 / 9 + 273.15, (val) => (val - 273.15) * 9 / 5 + 32),
-    new SimpleUnit(new Formats(["°R"]), (val) => val * 5 / 9, (val) => val * 9 / 5),
+    new SimpleUnit(new Formats(["K"], ["Kelvin"]), (val) => val, (val) => val),
+    new SimpleUnit(new Formats(["°C"], {
+        sg: ["degree Celsius"],
+        pl: ["degrees Celsius"],
+    }), (val) => val + 273.15, (val) => val - 273.15),
+    new SimpleUnit(new Formats(["°F"], {
+        sg: ["degree Fahrenheit"],
+        pl: ["degrees Fahrenheit"],
+    }), (val) => (val - 32) * 5 / 9 + 273.15, (val) => (val - 273.15) * 9 / 5 + 32),
+    new SimpleUnit(new Formats(["°R"], {
+        sg: ["degree Rankine"],
+        pl: ["degrees Rankine"],
+    }), (val) => val * 5 / 9, (val) => val * 9 / 5),
 )
 
 export default temperature;
