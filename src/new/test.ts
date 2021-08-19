e046f3c4e35316a0f591f25cd91cd758f79f9090
harrydehix <harry.holly02@gmail.com>
2021-08-19 18:51:56 +0200

    Update performance tests

--- src/new/test.ts
+++ src/new/test.ts
@@ -3,25 +3,38 @@ import units from "./units/units";
 import unitsOld from "../old/units/units";
 import convert1 from "convert";
 import convert2 from "convert-units";
+import { Symbols } from "../old/Collection";
 
 function unitjs() {
     units.from(12, "°C").to("°F");
+    units.from(100, "m").to("inch");
+    units.from(-12, "in²").to("m²");
 }
 
 function math() {
     (12 * 9 / 5) + 32;
+    (100 * 39.3701);
+    - 12 / 1550;
 }
 
 function convert() {
     convert1(12, "C").to("K");
+    convert1(100, "m").to("inch");
 }
 
 function convertUnits() {
     convert2(12).from("C").to("K");
+    convert2(100).from("m").to("in");
+    convert2(-12).from("in2").to("m2");
 }
 
+unitsOld.settings = {
+    symbols: Symbols.ALL
+}
 function oldUnitjs() {
     unitsOld.from(12, "°C").to("°F");
+    unitsOld.from(100, "m").to("inch");
+    unitsOld.from(-12, "in²").to("m²");
 }
 
-compareFunctions(convertUnits, unitjs, oldUnitjs, math);
+compareFunctions(unitjs, convertUnits, oldUnitjs, math);
