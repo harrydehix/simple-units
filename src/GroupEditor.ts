b2c58bffd544535be611de8bb267582b936fb105
harrydehix <harry.holly02@gmail.com>
2021-10-04 21:55:18 +0200

    Implement GroupEditor.select(), document GroupEditor

--- src/GroupEditor.ts
+++ src/GroupEditor.ts
@@ -18,20 +18,40 @@ export default class GroupEditor {
     }
 
     /**
-     * 
-     * @param units 
+     * Removes any unit except the given ones.
+     * @param units the units that shouldn't get removed
+     */
+    select(...units: string[]) {
+        const unitsToRemove: string[] = [];
+        for (const entry of this.group._internal._units()) {
+            const key = entry[0];
+            if (!units.includes(key)) unitsToRemove.push(key);
+        }
+        this.remove(...unitsToRemove);
+    }
+
+    /**
+     * Adds the given units to the related group. Units having the same _default notation_ (the notation that gets returned on
+     * calling `unit.toString()`) are overwritten.
+     * @param units units to add
      */
     add(...units: (FlexibleUnit | Unit)[]) {
         const hasCollection = this.group.collection !== Collection.None;
         for (const unit of units) {
             if (unit instanceof FlexibleUnit) {
+                // Handling "Flexible Units" differently (actually a flexible unit is just an object holding an array of common units, therefore it's array is added)
                 this.add(...unit._units);
             } else {
+                // Set the units group
                 unit.group = this.group;
+
+                // Remove units having the same default notation
                 if (this.group._internal._units().get(unit.toString())) {
                     this.remove(unit.toString());
                 }
-                const keys = unit.format.short.concat(unit.format.long.pl, unit.format.long.sg);
+
+                // Mapping the unit to it's notations
+                const keys = unit.computeNotations();
                 for (const key of keys) {
                     this.group._internal._units().set(key, unit);
                     if (hasCollection) this.group.collection._internal._setUnit(key, unit);
@@ -40,12 +60,20 @@ export default class GroupEditor {
         }
     }
 
+    /**
+     * Removes the given units from the related group. Only a single notation per unit is required.
+     * @param units units to remove
+     */
     remove(...units: string[]) {
         const hasCollection = this.group.collection !== Collection.None;
         for (const unit of units) {
+            // Resolving the unit object belonging to the key/notation
             const resolvedUnit = this.group._internal._units().get(unit);
             if (resolvedUnit) {
-                const keys = resolvedUnit.format.short.concat(resolvedUnit.format.long.pl, resolvedUnit.format.long.sg);
+                // Computing the unit's notations
+                const keys = resolvedUnit.computeNotations();
+
+                // Removing every related entry in the group (and collection)
                 for (const key of keys) {
                     this.group._internal._units().delete(key);
                     if (hasCollection) this.group.collection._internal._deleteUnit(key);
