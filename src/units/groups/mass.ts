a64dc663912b8d5fe1b6e593981dfa1c2b3ecf6b
Henri Hollmann <harry.holly02@gmail.com>
2024-09-28 10:59:58 +0200

    Fix typo in unit group 'mass'

--- src/units/groups/mass.ts
+++ src/units/groups/mass.ts
@@ -9,41 +9,68 @@ import siPrefixes from "../variables/siPrefixes";
 const mass = new Group("mass");
 
 mass.Editor.add(
-    new FlexibleUnit({
-        short: ["%0g"],
-        long: {
-            sg: ["%0gram", "%0gramme"],
-            pl: ["%0gram", "%0grams"],
-        }
-    }, 1, 0, "metric", [siPrefixes]),
-    new FlexibleUnit({
-        short: ["%0mt"],
-        long: {
-            sg: ["%0metric ton"],
-            pl: ["%0metric tons"],
-        }
-    }, 1e6, 0, "metric", [siPrefixes.subcopy("da")]),
-    new Unit({
-        short: ["t"],
-        long: {
-            sg: ["ton", "short ton"],
-            pl: ["tons", "short tons"],
-        }
-    }, 907_184.74, 0, "imperial"),
-    new Unit({
-        short: ["oz", "oz av"],
-        long: {
-            sg: ["ounce", "avoirdupois ounce"],
-            pl: ["ounces", "avoirdupois ounces"],
-        }
-    }, 28.349523125, 0, "imperial"),
-    new Unit({
-        short: ["lb", "lb av"],
-        long: {
-            sg: ["pound", "avoirdupois pound", "pound-mass"],
-            pl: ["pounds", "avoirdupois pounds"],
-        }
-    }, 453.59237, 0, "imperial"),
+    new FlexibleUnit(
+        {
+            short: ["%0g"],
+            long: {
+                sg: ["%0gram", "%0gramme"],
+                pl: ["%grams", "%0grammes"],
+            },
+        },
+        1,
+        0,
+        "metric",
+        [siPrefixes]
+    ),
+    new FlexibleUnit(
+        {
+            short: ["%0mt"],
+            long: {
+                sg: ["%0metric ton"],
+                pl: ["%0metric tons"],
+            },
+        },
+        1e6,
+        0,
+        "metric",
+        [siPrefixes.subcopy("da")]
+    ),
+    new Unit(
+        {
+            short: ["t"],
+            long: {
+                sg: ["ton", "short ton"],
+                pl: ["tons", "short tons"],
+            },
+        },
+        907_184.74,
+        0,
+        "imperial"
+    ),
+    new Unit(
+        {
+            short: ["oz", "oz av"],
+            long: {
+                sg: ["ounce", "avoirdupois ounce"],
+                pl: ["ounces", "avoirdupois ounces"],
+            },
+        },
+        28.349523125,
+        0,
+        "imperial"
+    ),
+    new Unit(
+        {
+            short: ["lb", "lb av"],
+            long: {
+                sg: ["pound", "avoirdupois pound", "pound-mass"],
+                pl: ["pounds", "avoirdupois pounds"],
+            },
+        },
+        453.59237,
+        0,
+        "imperial"
+    )
 );
 
 export default mass;
